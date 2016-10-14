770a47e8525f5e23a032b0112fd84f3af67ee78a
Marcio <taboca@gmail.com>
2016-10-14 17:28:45 -0300

    new path

--- 30_electron_test/main.js
+++ 30_electron_test/main.js
@@ -5,8 +5,8 @@ let mainWindow;
 app.on('ready', () => {
   mainWindow = new BrowserWindow({
       height: 600,
-      width: 800
+      width: 800, frame: false
   });
 
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
+  mainWindow.loadURL('http://localhost:8888/static/10_wrappers/wide.html');
 });
