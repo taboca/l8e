299616b083b3a0914bafd3807515bf5ba2dd48d9
Marcio <taboca@gmail.com>
2016-10-15 14:38:45 -0300

    fetch monitor project

--- 30_electron_test/main.js
+++ 30_electron_test/main.js
@@ -4,8 +4,8 @@ let mainWindow;
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({
-      height: 1080,
-      width: 3840, frame: false
+      height: 600,
+      width: 800
   });
 
   mainWindow.loadURL('http://localhost:8888/static/10_wrappers/wide.html');
