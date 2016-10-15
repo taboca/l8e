f5b2469a2b99de19e2a2ad0c50eb2eef3e0add56
Marcio <taboca@gmail.com>
2016-10-15 17:54:54 -0300

    Install note

--- 10_ubuntu-16.04/20_node.sh
+++ 10_ubuntu-16.04/20_node.sh
@@ -2,6 +2,13 @@ apt-get update
 apt-get install git
 apt-get install npm
 ln -s /usr/bin/nodejs /usr/bin/node
+node --version
+echo "If you have older node, you may need to consider this patch:"
+echo "sudo npm cache clean -f"
+echo "sudo npm install -g n"
+echo "sudo n stable"
+echo "sudo sudo n <version>" 
+echo "node -v"
 
 
 
