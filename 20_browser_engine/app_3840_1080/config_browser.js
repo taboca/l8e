987e3d198456e06335b46a4642bf62e61cb26401
Marcio <taboca@gmail.com>
2016-10-18 20:11:27 -0200

    Patch reverse

--- 20_browser_engine/app_3840_1080/config_browser.js
+++ 20_browser_engine/app_3840_1080/config_browser.js
@@ -1,5 +1,5 @@
 var config = {
-   'page'   : 'http://localhost:8888/static/10_wrappers/wide.html',
+   'page'   : 'http://localhost:8888/static/10_wrappers/3840-1080-reverse.html',
    'width'  : 3840,
    'height' : 1080,
    'frame'  : false
