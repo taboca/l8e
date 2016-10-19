808d0327b2db90ca677d4d0de3ad6d0efa63bdaa
Marcio <taboca@gmail.com>
2016-10-19 12:02:32 -0200

    new pages

--- 20_browser_engine/app_1920_1080_a/config_browser.js
+++ 20_browser_engine/app_1920_1080_a/config_browser.js
@@ -1,5 +1,5 @@
 var config = {
-   'page'   : 'http://localhost:8888/static/10_wrappers/left.html',
+   'page'   : 'http://localhost:8888/static/10_wrappers/1080-1920-grade-reverse.html',
    'width'  : 1920,
    'height' : 1080,
    'frame'  : false
